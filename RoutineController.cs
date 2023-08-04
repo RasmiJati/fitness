0645c19dcfe8279decf160928b98f4dd90495a30
Rasmi Jati <jatirasmi65@gmail.com>
2023-08-04 09:17:58 +0545

    perform edit operation for routine

--- RoutineController.cs
+++ RoutineController.cs
@@ -41,9 +41,9 @@ namespace fitness
                     case "3":
                         Delete();
                         break;
-                    //case "4":
-                    //    Edit();
-                    //    break;
+                    case "4":
+                        Edit();
+                        break;
                     default:
                         return;
                 }
@@ -140,6 +140,75 @@ namespace fitness
             {
                 routineRepository.Delete(routine);
                 Console.WriteLine("Deletion Sucessfull");
+            }
+        }
+
+        public void Edit()
+        {
+            int id = 0;
+            User user = null;
+            string startTime = null;
+            string endtime = null;
+            string sets = null;
+            string reps = null;
+            DateTime stime;
+            DateTime etime;
+
+            Console.WriteLine("Enter id to edit :");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            Routine r = routineRepository.ShowById(id);
+            if (r == null)
+            {
+                Console.WriteLine("Routine with id " + id + " not found");
+            }
+
+            else {
+                List<User> userList = userRepository.ShowAll();
+                if (user == null)
+                {
+                    Console.WriteLine("----------------------");
+                    Console.WriteLine("User List :");
+                    foreach (User u in userList)
+                        Console.WriteLine(u);
+                    Console.WriteLine("----------------------");
+
+                    int userId = 0;
+                    while (userId <= 0)
+                    {
+                        Console.WriteLine("Enter user id :");
+                        userId = Convert.ToInt32(Console.ReadLine());
+                        user = userRepository.ShowById(userId);
+                    }
+                }
+
+
+                Console.WriteLine("Enter start date time (yyyy-mm-dd hh:mm:ss): ");
+                if (startTime == null)
+                    startTime = Console.ReadLine();
+                stime = Convert.ToDateTime(startTime);
+
+
+                Console.WriteLine("Enter end date time (yyyy-mm-dd hh:mm:ss): ");
+                if (endtime == null)
+                    endtime = Console.ReadLine();
+                etime = Convert.ToDateTime(endtime);
+
+                Console.WriteLine("Enter sets : ");
+                if (sets == null)
+                {
+                    sets = Console.ReadLine();
+                }
+
+                Console.WriteLine("Enter reps : ");
+                if (reps == null)
+                {
+                    reps = Console.ReadLine();
+                }
+
+                Routine routine = new Routine(id, user, stime, etime, sets, reps);
+                routineRepository.Edit(routine);
+                Console.WriteLine("Edited Successfully");
             }
         }
     }
